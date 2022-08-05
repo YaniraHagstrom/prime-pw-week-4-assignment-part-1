a2eb0a99220ed403fdfdb40c37c1472e6b53efda
YaniraHagstrom <yanirahagstrom@gmail.com>
2022-08-05 14:30:14 -0500

    Completed assignment

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -19,11 +19,10 @@ console.log('Test - should say "Hello World!"', hello());
 console.log('Question 2:');
 
 function helloName( name ) {
-  console.log('Hello,', name, '!');
-  return;
+  return "Hello " + name;
 }
 // Remember to call the function to test
-helloName('Yani');
+console.log(helloName('Yani'));
 
 
 // 3. Function to add two numbers together & return the result
@@ -31,14 +30,12 @@ console.log('Question 3:');
 
 function addNumbers( firstNumber, secondNumber ) {
   let answer = firstNumber + secondNumber;
-  console.log('adding', firstNumber, 'and', secondNumber, 'equals', answer);
-  // return firstNumber + secondNumber;
-  return answer;
+  return 'the sum of the first and second value is ' + answer;
 }
 
-addNumbers( 9, 7);
-addNumbers(100, 450);
-addNumbers(-2, 10);
+console.log(addNumbers( 9, 7));
+console.log(addNumbers(100, 450));
+console.log(addNumbers(-2, 10));
 
 
 // 4. Function to multiply three numbers & return the result
@@ -46,8 +43,7 @@ console.log('Question 4:');
 
 function multiplyThree(num1, num2, num3 ){
   let product = num1 * num2 * num3;
-  console.log('multiplying', num1,',', num2, '&', num3, 'equals', product);
-  return product;
+  return 'The product of the three numbers is ' + product;
 }
 
 console.log(multiplyThree(2,3,5));
@@ -76,9 +72,8 @@ console.log('Question 6:');
 
 function getLast( array ) {
   if ( array.length > 0){
-    console.log('the arrays length is', array.length);
     let lastItem = array.length - 1;
-    return array[lastItem];
+    return 'The last item in the array is ' + array[lastItem];
   }
   return 'undefined';
 
@@ -116,7 +111,10 @@ console.log('this should return false',find(1, [2, 5, 7, 4 ,7]));
 console.log('Question 8:');
 
 function isFirstLetter(letter, string) {
-
+  if ( letter === string[0]){
+    return true;
+  }
+  return false;
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
@@ -124,21 +122,59 @@ console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 // 9. Function to return the sum of all numbers in an array
 console.log('Question 9:');
 
-function sumAll( ) {
+function sumAll(array) {
   let sum = 0
   // TODO: loop to add items
+  for (let i = 0; i < array.length; i++){
+    sum+= array[i];
+  }
   return sum;
 }
+
+console.log('the sum of all of the items in this array is', sumAll([1, 2, 3, 4]));
+console.log('the sum of all of the items in this array is', sumAll([10, 20, 2, 3, 4]));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
 console.log('Question 10:');
 
+function positiveArray (array2){
+  let newArray = [];
+  for (let i = 0; i < array2.length; i++){
+    if ( array2[i] > 0){
+      newArray.push(array2[i]);
+    }
+  }
+  return newArray;
+}
 
-
+console.log(positiveArray([10, -20, 2, -3, 4, 75]));
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
 //     here in a comment, write the function, and test it!
 console.log('Question 11:');
+
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+function whichQuarter(month){
+  if ( month <= 0){
+    return 'That month does not exit';
+  }
+  if ( month < 5){
+    return 'This month is in the first quarter of the year';
+  }
+  else if (month > 4 && month < 9){
+    return 'This month is in the second quarter of the year';
+  }
+  else{
+    return 'This month is in the third quarter of the year';
+  }
+}
+
+console.log(whichQuarter(1));
+console.log(whichQuarter(5));
+console.log(whichQuarter(12));
